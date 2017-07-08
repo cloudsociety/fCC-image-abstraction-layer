79339c686dc836b3fc1569c286f17a98f6224159
David Cross (MBP) <david@cloudsociety.com>
2017-07-08 15:25:23 +0800

    Add search functionality to /search/:term

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('./config/config.js');
 
+// var _ = require('lodash');
+var axios = require('axios');
 var express = require('express');
 const app = express();
 
@@ -10,11 +12,27 @@ app.use(express.static('public'));
 app.get('/search/:term', (req,res) => {
   var latestTerms = getLatestSearches();
   var searchTerm = req.params.term;
+  var page = req.query.offset ? (req.query.offset*10)+1 : 1;
 
   latestTerms.latest_searches.push(searchTerm);
   setLatestSearches(latestTerms);
 
-  res.send('search works');
+  // `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE}&q=${searchTerm}&searchType=image`
+  const url = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_SEARCH_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE}&q=${searchTerm}&searchType=image&start=${page}`;
+  axios.get(url).then((response) => {
+    var items = response.data.items;
+    items = items.map((item) => {
+      return {
+        url: item.link,
+        snippet: item.snippet,
+        thumbnail: item.image.thumbnailLink,
+        context: item.image.contextLink
+      }
+    });
+    res.status(200).send(items);
+  }).catch((error) => {
+    res.status(400).send(error);
+  });
 });
 
 app.get('/latest', (req, res) => {
